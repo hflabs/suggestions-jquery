b90d4e7b6d5860c4e2d6bbebb6d74cad1396b407
dp <d.podolkhov@simbirsoft.com>
2014-01-31 19:05:39 +0400

    updates selection on space key pressed

--- suggestions-jquery.js
+++ suggestions-jquery.js
@@ -31,8 +31,27 @@
         options = $.extend({
             dataType: 'jsonp'
         }, options || {});
+        
+        options.transformResult = function (response) {
+            var result = typeof response === 'string' ? $.parseJSON(response) : response;
+            if (!result.suggestions) {
+                result.suggestions = [];
+            }
+            return result;
+        };
 
         Autocomplete.call(this, el, options);
+    };
+
+    var keys = {
+        ESC: 27,
+        TAB: 9,
+        RETURN: 13,
+        SPACE: 32,
+        LEFT: 37,
+        UP: 38,
+        RIGHT: 39,
+        DOWN: 40
     };
 
     Suggestions.prototype = $.extend({}, Autocomplete.prototype, {
@@ -43,8 +62,80 @@
          */
         isBadQuery: function (q) {
             return false;
+        },
+        
+        onKeyPress: function (e) {
+            var that = this;
+
+            // If suggestions are hidden and user presses arrow down, display suggestions:
+            if (!that.disabled && !that.visible && e.which === keys.DOWN && that.currentValue) {
+                that.suggest();
+                return;
+            }
+
+            if (that.disabled || !that.visible) {
+                return;
+            }
+
+            switch (e.which) {
+                case keys.ESC:
+                    that.el.val(that.currentValue);
+                    that.hide();
+                    break;
+                case keys.RIGHT:
+                    if (that.hint && that.options.onHint && that.isCursorAtEnd()) {
+                        that.selectHint();
+                        break;
+                    }
+                    return;
+                case keys.TAB:
+                    if (that.hint && that.options.onHint) {
+                        that.selectHint();
+                        return;
+                    }
+                    // Fall through to RETURN
+                case keys.RETURN:
+                    if (that.selectedIndex === -1) {
+                        that.hide();
+                        return;
+                    }
+                    that.select(that.selectedIndex);
+                    if (e.which === keys.TAB && that.options.tabDisabled === false) {
+                        return;
+                    }
+                    break;
+                case keys.SPACE:
+                    that.updateValue(that.selectedIndex);
+                    return;
+                case keys.UP:
+                    that.moveUp();
+                    break;
+                case keys.DOWN:
+                    that.moveDown();
+                    break;
+                default:
+                    return;
+            }
+
+            // Cancel event if function did not return:
+            e.stopImmediatePropagation();
+            e.preventDefault();
+        },
+
+        /**
+         * Selects value and continues selecting
+         */
+        updateValue: function(i, shouldIgnoreNextValueChange){
+            var that = this,
+                selectedValue = that.suggestions[i];
+
+            if (selectedValue) {
+                that.el.val(selectedValue);
+                that.ignoreValueChange = shouldIgnoreNextValueChange;
+                that.onSelect(i);
+            }
         }
-            
+        
     });
     
     $.fn.suggestions = function(options, args){
