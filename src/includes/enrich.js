9b55fe828e2afd47980e01cfc59808a9c6b56ec6
Anton Zhiyanov <m@antonz.ru>
2014-08-18 14:06:21 +0400

    sug-282 ignore "enriched" suggestion from the server if it is not really enriched

--- src/includes/enrich.js
+++ src/includes/enrich.js
@@ -12,6 +12,12 @@
                         var that = this,
                             resolver = $.Deferred();
 
+                        function suggestionIsDefined(suggestion) {
+                            return suggestion && suggestion.data
+                                && suggestion.data.hasOwnProperty('qc')
+                                && suggestion.data.qc !== null;
+                        }
+
                         // if current suggestion is already enriched, use it
                         if (suggestion.data && suggestion.data.qc != null) {
                             return resolver.resolve(suggestion);
@@ -23,7 +29,15 @@
                                 that.enableDropdown();
                             })
                             .done(function (suggestions) {
-                                resolver.resolve(suggestions[0] || suggestion);
+                                var serverSuggestion = suggestions[0];
+                                if (suggestionIsDefined(serverSuggestion)) {
+                                    // return suggestion from dadata
+                                    resolver.resolve(serverSuggestion);
+                                } else {
+                                    // dadata is turned off on the server, ignore response
+                                    // and use suggestion selected by the user
+                                    resolver.resolve(suggestion);
+                                }
                             })
                             .fail(function () {
                                 resolver.resolve(suggestion);
