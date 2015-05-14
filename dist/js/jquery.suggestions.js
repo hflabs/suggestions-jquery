b96be33175f77f6b0f0fb3f5ebb239c714f22146
dunal <dp.job@mail.ru>
2015-05-14 16:16:16 +0300

    sug-505 absent INN fixed

--- dist/js/jquery.suggestions.js
+++ dist/js/jquery.suggestions.js
@@ -527,8 +527,8 @@
                 if (formattedInnOGRN || address || formattedFIO) {
                     value +=
                         '<div class="' + that.classes.subtext + '">' +
-                        '<span class="' + that.classes.subtext_inline + '">' + formattedInnOGRN + '</span>' +
-                        chooseFormattedField(address, formattedFIO) +
+                        '<span class="' + that.classes.subtext_inline + '">' + (formattedInnOGRN || '') + '</span>' +
+                        (chooseFormattedField(address, formattedFIO) || '') +
                         '</div>';
                 }
                 return value;
