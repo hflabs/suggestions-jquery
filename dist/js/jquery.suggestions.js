1f46562a30ebfd2f38a13946ab960baa865de947
dunal <dp.job@mail.ru>
2015-08-20 12:44:38 +0300

    sug-561 version 15.8

--- dist/js/jquery.suggestions.js
+++ dist/js/jquery.suggestions.js
@@ -1,5 +1,5 @@
 /**
- * DaData.ru Suggestions jQuery plugin, version 15.7.4
+ * DaData.ru Suggestions jQuery plugin, version 15.8.0
  *
  * DaData.ru Suggestions jQuery plugin is freely distributable under the terms of MIT-style license
  * Built on DevBridge Autocomplete for jQuery (https://github.com/devbridge/jQuery-Autocomplete)
@@ -725,7 +725,7 @@
 
     Suggestions.defaultOptions = defaultOptions;
 
-    Suggestions.version = '15.7.4';
+    Suggestions.version = '15.8.0';
 
     $.Suggestions = Suggestions;
 
