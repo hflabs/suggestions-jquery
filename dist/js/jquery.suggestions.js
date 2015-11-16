b987d945bf47454934c70f96a8c4c60a97af1a5a
dunal <dp.job@mail.ru>
2015-11-16 17:15:49 +0300

    respect country constraint

--- dist/js/jquery.suggestions.js
+++ dist/js/jquery.suggestions.js
@@ -1,5 +1,5 @@
 /**
- * DaData.ru Suggestions jQuery plugin, version 15.10.3
+ * DaData.ru Suggestions jQuery plugin, version 15.10.4
  *
  * DaData.ru Suggestions jQuery plugin is freely distributable under the terms of MIT-style license
  * Built on DevBridge Autocomplete for jQuery (https://github.com/devbridge/jQuery-Autocomplete)
@@ -747,7 +747,7 @@
 
     Suggestions.defaultOptions = defaultOptions;
 
-    Suggestions.version = '15.10.3';
+    Suggestions.version = '15.10.4';
 
     $.Suggestions = Suggestions;
 
