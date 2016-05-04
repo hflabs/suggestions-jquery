5ba1c8c41e81076329c116b2c461494af35c0e97
Anton Zhiyanov <m@antonz.ru>
2016-05-04 13:33:38 +0300

    sug-638 bump version

--- dist/js/jquery.suggestions.js
+++ dist/js/jquery.suggestions.js
@@ -1,5 +1,5 @@
 /**
- * DaData.ru Suggestions jQuery plugin, version 16.4.1
+ * DaData.ru Suggestions jQuery plugin, version 16.5.1
  *
  * DaData.ru Suggestions jQuery plugin is freely distributable under the terms of MIT-style license
  * Built on DevBridge Autocomplete for jQuery (https://github.com/devbridge/jQuery-Autocomplete)
@@ -779,7 +779,7 @@
 
     Suggestions.defaultOptions = defaultOptions;
 
-    Suggestions.version = '16.4.1';
+    Suggestions.version = '16.5.1';
 
     $.Suggestions = Suggestions;
 
