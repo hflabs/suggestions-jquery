2619a1deca508519870843e7275dba6a92c44ebe
Sergey V <volkovsergey1981@mail.ru>
2017-03-21 11:31:12 +0300

    sug-728 add test

--- test/specs/format_select_spec.js
+++ test/specs/format_select_spec.js
@@ -272,4 +272,85 @@ describe('Text to insert after selection', function () {
         expect(this.input.value).toEqual('г Москва ');
     });
 
+    it('Should show only city if city-settlement', function(){
+        var suggestions = [
+            {
+                unrestricted_value: 'Новосибирская обл, г Новосибирск, Заельцовский р-н, ул Вавилова',
+                value: 'г Новосибирск, ул Вавилова',
+                data: {
+                    postal_code: '630082',
+                    country: 'Россия',
+                    region_fias_id: '1ac46b49-3209-4814-b7bf-a509ea1aecd9',
+                    region_kladr_id: '5400000000000',
+                    region_with_type: 'Новосибирская обл',
+                    region_type: 'обл',
+                    region_type_full: 'область',
+                    region: 'Новосибирская',
+                    city_fias_id: '8dea00e3-9aab-4d8e-887c-ef2aaa546456',
+                    city_kladr_id: '5400000100000',
+                    city_with_type: 'г Новосибирск',
+                    city_type: 'г',
+                    city_type_full: 'город',
+                    city: 'Новосибирск',
+                    city_district_with_type: 'Заельцовский р-н',
+                    city_district_type: 'р-н',
+                    city_district_type_full: 'район',
+                    city_district: 'Заельцовский',
+                    street_fias_id: '989d3078-b3cd-4c9a-a6c1-f7c62035d8b2',
+                    street_kladr_id: '54000001000017800',
+                    street_with_type: 'ул Вавилова',
+                    street_type: 'ул',
+                    street_type_full: 'улица',
+                    street: 'Вавилова',
+                    fias_id: '989d3078-b3cd-4c9a-a6c1-f7c62035d8b2',
+                    fias_level: '7',
+                    kladr_id: '54000001000017800',
+                    capital_marker: '2',
+                    okato: '50401372000',
+                    oktmo: '50701000',
+                    tax_office: '5402',
+                    geo_lat: '55.0556543',
+                    geo_lon: '82.895762',
+                    qc_geo: '2'
+                }
+            }
+        ];
+
+        var $parent = $('<input>').appendTo($(document.body));
+
+        $parent.suggestions({
+            serviceUrl: serviceUrl,
+            type: 'ADDRESS',
+            geoLocation: false,
+            bounds: 'city-settlement',
+            mobileWidth: NaN
+        });
+
+        this.instance.setOptions({
+            type: 'ADDRESS',
+            geoLocation: false,
+            bounds: 'street',
+            constraints: $parent
+        });
+
+        // Setting type will request for status
+        helpers.returnGoodStatus(this.server);
+        this.server.requests.length = 0;
+
+        $parent.val('Ново');
+        this.input.value = 'Вави';
+        this.instance.onValueChange();
+
+        // Respond with suggestions with restricted values
+        this.server.respond(helpers.responseFor(suggestions));
+
+        // Selecting causes enrichment
+        this.instance.select(0);
+        this.server.respond(helpers.responseFor(suggestions));
+
+        expect($parent.val()).toEqual('г Новосибирск');
+
+        $parent.remove();
+    });
+
 });
