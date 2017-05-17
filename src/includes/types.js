1d51b5793b8a09f40c6c29214c2a754ddfca18ee
Anton Zhiyanov <m@antonz.ru>
2017-05-17 16:36:03 +0300

    sug-746 address restrictions and boosting for banks

--- src/includes/types.js
+++ src/includes/types.js
@@ -460,6 +460,8 @@ types['BANK'] = {
             'data.swift': null
         }
     })],
+    dataComponents: ADDRESS_COMPONENTS,
+    geoEnabled: true,
     formatResult: function (value, currentValue, suggestion, options) {
         var that = this,
             formattedBIC = that.highlightMatches(utils.getDeepValue(suggestion.data, 'bic'), currentValue, suggestion),
