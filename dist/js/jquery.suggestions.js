6e53919b9563edc85cefba29e12b5bc092f14941
Anton Zhiyanov <m@antonz.ru>
2018-06-19 12:58:48 +0300

    SUG-106 bump version

--- dist/js/jquery.suggestions.js
+++ dist/js/jquery.suggestions.js
@@ -1,5 +1,5 @@
 /**
- * DaData.ru Suggestions jQuery plugin, version 99.9.9
+ * DaData.ru Suggestions jQuery plugin, version 18.6.0
  *
  * DaData.ru Suggestions jQuery plugin is freely distributable under the terms of MIT-style license
  * Built on DevBridge Autocomplete for jQuery (https://github.com/devbridge/jQuery-Autocomplete)
@@ -4480,7 +4480,7 @@ notificator
 
 Suggestions.defaultOptions = DEFAULT_OPTIONS;
 
-Suggestions.version = '99.9.9';
+Suggestions.version = '18.6.0';
 
 $.Suggestions = Suggestions;
 
