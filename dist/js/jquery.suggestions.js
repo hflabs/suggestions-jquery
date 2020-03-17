0db08e605332fd9f002174b0aeff9dd0a5ce005e
Anton <m@antonz.ru>
2020-03-17 17:52:33 +0300

    SUG-1155 absolute position for suggestions list

--- dist/js/jquery.suggestions.js
+++ dist/js/jquery.suggestions.js
@@ -1,5 +1,5 @@
 /**
- * DaData.ru Suggestions jQuery plugin, version 20.2.1
+ * DaData.ru Suggestions jQuery plugin, version 20.2.2
  *
  * DaData.ru Suggestions jQuery plugin is freely distributable under the terms of MIT-style license
  * Built on DevBridge Autocomplete for jQuery (https://github.com/devbridge/jQuery-Autocomplete)
@@ -5259,7 +5259,7 @@ notificator.on("assignSuggestions", show);
 
 Suggestions.defaultOptions = DEFAULT_OPTIONS;
 
-Suggestions.version = "20.2.1";
+Suggestions.version = "20.2.2";
 
 $.Suggestions = Suggestions;
 
